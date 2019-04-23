630cc60efba5064c00e8243dd43e88aa257bd3a8
Solenn THOMAS <solenn.thomas@hotmail.fr>
2019-04-23 15:31:44 +0200

    initialisation des fichiers

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "struct_SGF.h"
+#include "primitive.h"
+#include "command_shell.h"
+#include "save_load_SGF.h"
 
 int main(int argc, char** argv){
 
