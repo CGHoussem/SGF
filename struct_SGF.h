87775de9d5e80ed7c05c4571c0f0acce8aed2983
Houssem Ben Mabrouk <cjhoussem@gmail.com>
2019-04-24 21:00:36 +0200

    Correction de la définition des structures

--- struct_SGF.h
+++ struct_SGF.h
@@ -3,41 +3,46 @@
 
 #include <time.h>
 
-typedef struct 
-{
-	char permissions[9] = {1,1,1,1,0,0,1,0,0};  // rwxr--r--
+typedef struct index index;
+typedef struct bloc_donnees bloc_donnees;
+typedef struct bloc_repertoire bloc_repertoire;
+typedef union bloc bloc;
+typedef struct inode inode;
+typedef struct disque disque;
+
+struct index {
+	char* nom;
+	inode* inode;
+};
+
+struct bloc_donnees {
+	char donnees[1024];
+	int taille_fichier;
+};
+
+struct bloc_repertoire {
+	index* index;
+};
+
+union bloc {
+	bloc_donnees b_donnees;
+	bloc_repertoire b_repertoire;
+};
+
+struct inode {
+	char permissions[9]; // rwxr--r-- {1,1,1,1,0,0,1,0,0}
 	int typefichier; // 1 = texte, 2 = binaire, 3 = repertoire
 	time_t date_creation;
 	time_t date_modification;
 	bloc bloc;
 	inode* inode_suivant;
-} inode;
+};
 
-typedef struct {
-	char donnees[1024];
-	int taille_fichier;
-} bloc_donnees;
-
-typedef struct {
-	index* index;
-} bloc_rep;
-
-typedef union{
-	bloc_donnees b_donnees;
-	bloc_rep b_repertoire;
-} bloc;
-
-typedef struct{
-	char* nom;
-	inode* inode;
-} index;
-
-typedef struct{
+struct disque {
 	inode* inodes;	//1er inode
 	int nb_inode;
 	bloc* blocs; //1er bloc
 	int nb_bloc;
-} disque;
-
+};
 
 #endif
