31afdd558c126910a4a1e38459a9d197f281be81
Tucpakic <hakim78260@hotmail.fr>
2019-06-08 19:29:21 +0200

    renommage et modif makefile

--- command_shell.h
+++ command_shell.h
@@ -1,8 +1,27 @@
 #ifndef COMMAND_SHELL
 #define COMMAND_SHELL
 
-//prototypes
+#include "constants.h"
+#include "struct_SGF.h"
 
-void printDir();
+char* readline();
+char** parse(char* input);
+int executeLine(Disk* disk, char* input,Inode* current_inode);
+//return the inode associated with the given path, NULL if the path doesn't exist
+Inode* path_to_inode(char* parsedInput,Inode* current_inode,Disk* disk); 
+//return the inode associated with the given path 
+//or create a file and return its inode if the last file doesn't exist 
+//or return NULL if a directory in the middle of the path doesn't exist
+Inode* path_to_destination(char* parsedInput,Inode* current_inode,Disk* disk);
+//return the inode associated with the given path 
+//or create a directory and return its inode if the last directory doesn't exist 
+//or return NULL if a directory in the middle of the path doesn't exist
+Inode* path_to_destination_directory(char* parsedInput,Inode* current_inode,Disk* disk);
+//count the number of path in parsedInput
+int count_path(char** parsedInput);
+//free the input
+void free_input(char* input,char** parsedInput);
+//converts the rights input into an array representing the rights
+char* convertRights(char* rights, int length, char permissions[9]);
 
 #endif
